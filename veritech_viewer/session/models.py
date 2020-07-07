a19e958657686e9c6f88de9bfc2c59e0be409252
jvent <justine.ventura14@gmail.com>
2020-07-07 00:04:39 -0600

    fix booklet template, add booklet class to Models

--- veritech_viewer/session/models.py
+++ veritech_viewer/session/models.py
@@ -6,3 +6,11 @@ class Session(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     booklet = models.PositiveIntegerField()
     status = models.PositiveIntegerField()
+
+class Booklet(models.Model):
+    BKLT_TYPE=((0, "10"),(1,"5"),(2,"4,3,3"))
+    booklet_type = models.CharField(max_length=1,choices=BKLT_TYPE, default = 0)
+    # questions and overall mark
+
+
+

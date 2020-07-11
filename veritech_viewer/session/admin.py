ab40ea8141fa156ab1aa6a733a2673bf9dd5add9
root@veritech.jimgao.tk <root@veritech.jimgao.tk>
2020-07-11 21:07:34 +0000

    Added django admin stuff

--- veritech_viewer/session/admin.py
+++ veritech_viewer/session/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import Session, Booklet, Page, Question
+
 # Register your models here.
+
+class SessionAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', ]
+    fields = ['student_id', 'timestamp', 'status']
+    list_display = ['id', 'student_id', 'timestamp', 'status']
+
+class BookletAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', ]
+    fields = ['session', 'booklet_type']
+    list_display = ['id', 'session', 'booklet_type']
+
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', ]
+    fields = ['booklet', 'page_number', 'overall_mark']
+    list_display = ['id', 'booklet', 'page_number', 'overall_mark']
+
+class QuestionAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', ]
+    fields = ['page', 'marking_outcome', 'correct_ans', 'pred_regex']
+    list_display = ['id', 'page', 'marking_outcome']
+
+
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Booklet, BookletAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Question, QuestionAdmin)
+

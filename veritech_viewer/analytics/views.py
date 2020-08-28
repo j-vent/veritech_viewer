b616424380c9da8e858b312c5bece1f806ced2ca
jvent <justine.ventura14@gmail.com>
2020-08-27 21:46:12 -0600

    add trends for errors and scores

---
+++ veritech_viewer/analytics/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from session.models import Session, Booklet, Page, Question
+
+def analytics(request):
+    errors = {}
+    scores = {}
+    error_vals = []
+    score_vals = []
+    studentID_Query = request.GET.get('student_id', '')
+    for i in range(1,13):
+        scores[i] = []
+        errors[i] = []
+    print(scores)
+
+    if studentID_Query != '':
+
+        filtered_sessions = Session.sessions.all().filter(student_id=studentID_Query).filter(status=1)
+        for session in filtered_sessions:
+            num_errors = 0
+            booklet_score = 0
+
+            # months.append(session.timestamp.month);
+            filtered_booklets = Booklet.booklets.all().filter(session__in=filtered_sessions)
+
+            filtered_pages = Page.pages.all().filter(booklet__in=filtered_booklets)
+
+            for pages in filtered_pages:
+                # sum up average mark of all pages in a booklet
+                #scores[session.timestamp.month] += (pages.overall_mark/len(filtered_pages))/ len(scores[session.timestamp.month] +)
+                scores[session.timestamp.month].append(pages.overall_mark / len(filtered_pages));
+            filtered_questions = Question.questions.all().filter(page__in=filtered_pages)
+
+            for question in filtered_questions:
+                if question.marking_outcome == "REJECT":
+                    num_errors += 1
+            # sum up average number of errors for a booklet
+            # errors[session.timestamp.month] += (num_errors/len(filtered_booklets))/ len(errors[session.timestamp.month])
+            errors[session.timestamp.month].append(num_errors / len(filtered_booklets))
+
+
+        for val in errors.values():
+            if len(val) == 0:
+                error_vals.append(0)
+            else:
+                error_vals.append(sum(val)/len(val))
+        print(error_vals)
+        
+        for score in scores.values():
+            if len(score) == 0:
+                score_vals.append(0)
+            else:
+                score_vals.append(sum(score)/len(score) * 100)
+        
+    return render(request, "analytics.html", {'student_id': studentID_Query,
+                                              'error_labels': list(errors.keys()),
+                                              'error_data': error_vals,'score_labels': list(scores.keys()),
+                                              'score_data': score_vals})
+
